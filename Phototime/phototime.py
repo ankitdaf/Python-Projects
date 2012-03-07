4ba008a4d75dfaa09385d5fcbb73316ce74f7c41
Ankit Daftery <ankitdaf@gmail.com>
2012-03-07 08:38:23 +0530

    Added counter to filenames,no more skipping duplicates

--- Phototime/phototime.py
+++ Phototime/phototime.py
@@ -3,9 +3,16 @@ import sys,os, time,subprocess
 path = sys.argv[1]
 ftype = sys.argv[2]
 filelist = subprocess.Popen("find " + path + " -name \"*."+ftype+"\"" , shell=True,stdout=subprocess.PIPE, cwd=None).stdout.read().strip('\n').split('\n')
+j=1
 for i in filelist:
 	statinfo = os.stat(i)
-	newname= time.strftime("%Y-%m-%d %H.%M.%S", time.localtime(statinfo.st_mtime)) + ".%s"%(sys.argv[2])
-	tempnewname = i.rsplit('/',1)[0] +'/'+ newname
+	newname= i.rsplit('/',1)[0] +'/'+ time.strftime("%Y-%m-%d %H.%M.%S", time.localtime(statinfo.st_mtime))
+	tempnewname =  newname +  ".%s"%(sys.argv[2])
+	print tempnewname
 	if(not os.path.exists(tempnewname)):
 		os.renames(i,tempnewname)
+		j=1
+	else:
+		tempnewname = newname + str(j) + ".%s"%(sys.argv[2])
+		os.renames(i,tempnewname)
+		j+=1
